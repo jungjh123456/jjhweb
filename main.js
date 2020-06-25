0f2c9f68ec20f0cafb363af3f6070dacc29eff16
APPLE <apple@APPLEui-MacBookPro.local>
2020-06-25 22:33:27 +0900

    main.js

--- main.js
+++ main.js
@@ -1,25 +1,43 @@
-(function (window, document) {
-'use strict';
-
-const $login = document.querySelectorAll('.sign-box');
-const $loginBtn = document.querySelectorAll('.login-btn');
+const open = document.querySelector('.login-btn');
+const modal = document.querySelector('.sign-box');
 
 
-$loginBtn.onclick = function() {
-    btnElements();
+open.onclick = () => {
+    modal.style.display = "flex";
 }
 
+document.body.addEventListener('onclick', function(){
+    if(modal.style.display = "flex"){
+        modal.style.display = "none";
+    }
+})
 
-function btnElements(){
-    [].forEach.call($login, function (btn) {
-        btn.classList.toggle('on');
-    });
 
-}
 
-function offElements(){
-    [].forEach.call($login, function(btn) {
-        btn.classList.remove('on');
-    })
-}
-})(window, document);
+
+
+// (function (window, document) {
+// 'use strict';
+
+// const $login = document.querySelectorAll('.sign-box');
+// const $loginBtn = document.querySelectorAll('.login-btn');
+
+
+// $loginBtn.onclick = function() {
+//     btnElements();
+// }
+
+
+// function btnElements(){
+//     [].forEach.call($login, function (btn) {
+//         btn.classList.toggle('on');
+//     });
+
+// }
+
+// function offElements(){
+//     [].forEach.call($login, function(btn) {
+//         btn.classList.remove('on');
+//     })
+// }
+// })(window, document);
